d8ee4bbe9fc485077522ddb4345df306a833bfa3
integerJI <jjs9536@gmail.com>
2020-11-07 00:02:52 +0900

    [Fix] 메인페이지 진입 시 자동 로그아웃 처리, 회원가입 후 login 페이지 이동 로직 수정

--- user/views.py
+++ user/views.py
@@ -7,11 +7,11 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
                 request.POST['username'], password=request.POST['password1'])
-            auth.login(request, user)
             return redirect('login')
     return render(request, 'signup.html')
 
 def login(request):
+    auth.logout(request)
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
